7b2475802f7df2147abe1a173794ca615162f7f1
sushil-sharma01 <paudelmssp@gmail.com>
2025-12-27 17:42:00 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // used to provide popup in browser
+console.log(3+
+    3 ) // code redability should need to be high
+console.log("Hii there")
+
+let name = "Rahul"
+let age = 22
+let isLoggedIn = false //boolean
+// number => 2 to pow 53
+//bigint => for big no like in trading
+//string => " "
+//boolean => true/false
+//null => sandalone value - representation of empty value
+// undefined => not assigned value yet
+// symbol => to find uniqueness
+
+//object 
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
+console.log(typeof "Rahul"); // string
